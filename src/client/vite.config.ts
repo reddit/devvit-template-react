8c9b730b997cac047cfde1257f7785f9f4c5a2e9
Marcus Wood <marcus.wood@reddit.com>
2025-11-12 10:52:31 -0500

    empty out dir

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwind()],
   build: {
     outDir: '../../dist/client',
+    emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
       input: {
