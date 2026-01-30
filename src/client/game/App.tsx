32750f07b5707feeebac4270db92a7b1ac116387
Marcus Wood <marcus.wood@reddit.com>
2026-01-30 16:44:07 -0500

    migrate to vite and hono bump deps, clean up configs

--- src/client/game/App.tsx
+++ src/client/game/App.tsx
@@ -5,14 +5,21 @@ export const App = () => {
   const { count, username, loading, increment, decrement } = useCounter();
   return (
     <div className="flex relative flex-col justify-center items-center min-h-screen gap-4">
-      <img className="object-contain w-1/2 max-w-[250px] mx-auto" src="/snoo.png" alt="Snoo" />
+      <img
+        className="object-contain w-1/2 max-w-[250px] mx-auto"
+        src="/snoo.png"
+        alt="Snoo"
+      />
       <div className="flex flex-col items-center gap-2">
         <h1 className="text-2xl font-bold text-center text-gray-900 ">
           {username ? `Hey ${username} 👋` : ''}
         </h1>
         <p className="text-base text-center text-gray-600 ">
-          Edit <span className="bg-[#e5ebee]  px-1 py-0.5 rounded">src/client/game/App.tsx</span> to
-          get started.
+          Edit{' '}
+          <span className="bg-[#e5ebee]  px-1 py-0.5 rounded">
+            src/client/game/App.tsx
+          </span>{' '}
+          to get started.
         </p>
       </div>
       <div className="flex items-center justify-center mt-5">
